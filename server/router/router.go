75c7a1e1a24d66db1d0bebe5ace5a55daac73ea3
madhav <eragon.cube@outlook.com>
2025-04-06 12:53:45 +0530

    Create Router

--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,18 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/madhavarora03/todo-list/middleware"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/task/undo/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/task", middleware.DeleteAllTasks).Methods("DELETE", "OPTIONS")
+
+	return router
+}
